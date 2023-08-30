e9efe0b6e4e3815883c3c8d2d71fe9de1b7f9c53
George Jimy <knivespeans@gmail.com>
2023-08-29 19:58:02 -0500

    Constructor

---
+++ Progra y Produ/Ejercicio1/Ejercicio1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Pokemon
+{
+    private string name;
+    private float weight;
+    private int exp;
+
+    public Pokemon(string name, float weight, int exp) 
+    {
+        this.name = name;
+        this.weight = weight;
+        this.exp = exp;
+    }
+
+}
+
+class program
+{
+    static void Main(string[] args)
+    {
+        Pokemon charmander = new Pokemon("Charmander", 8.5f, 1500);
+
+    }
+}
