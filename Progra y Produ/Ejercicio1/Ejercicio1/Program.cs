11f2a67e39bc9a36564c975a27709e91aadcc655
George Jimy <knivespeans@gmail.com>
2023-08-29 20:07:07 -0500

    Herencia

--- Progra y Produ/Ejercicio1/Ejercicio1/Program.cs
+++ Progra y Produ/Ejercicio1/Ejercicio1/Program.cs
@@ -1,25 +1,33 @@
 ﻿using System;
 
-public class Pokemon
+class Type
+{
+    public string type;
+}
+
+public class Pokemon:Type
 {
     private string name;
     private float weight;
     private int exp;
 
-    public Pokemon(string name, float weight, int exp) 
+    public Pokemon(string name, float weight, int exp,string type) 
     {
         this.name = name;
         this.weight = weight;
         this.exp = exp;
+        this.type = type;
     }
 
 }
+
+
 
 class program
 {
     static void Main(string[] args)
     {
-        Pokemon charmander = new Pokemon("Charmander", 8.5f, 1500);
-
+        Pokemon charmander = new Pokemon("Charmander", 8.5f, 1500,"fire");
+        
     }
 }
